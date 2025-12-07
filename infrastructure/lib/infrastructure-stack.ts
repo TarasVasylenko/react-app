9c7d77537189690494a3affa134033f66b2165df
Taras Vasylenko <user@Tarass-Laptop.local>
2025-12-07 17:24:10 +0200

    first commit

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -5,7 +5,7 @@ import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
 
-export class SecureWebStack extends cdk.Stack {
+export class InfrastructureStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
         const ReactAppBucket = new s3.Bucket(this, 'ReactAppBucket', {
